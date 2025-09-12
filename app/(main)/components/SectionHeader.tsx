dfd2b48afa3f853052a7bb9cba209fcbe6b87b02
MystiaFin <cytrsx01@gmail.com>
2025-09-12 16:03:32 +0700

    metadata adjustment

--- app/(main)/components/SectionHeader.tsx
+++ app/(main)/components/SectionHeader.tsx
@@ -1,24 +1,22 @@
 type SectionHeaderProps = {
   title: string;
-  h2ClassName?: string; // custom classes for <h2>
-  hrClassName?: string; // custom classes for <hr>
+  h2ClassName?: string;
+  hrClassName?: string;
 };
 
 export default function SectionHeader({
   title,
-  h2ClassName = '', // defaults to nothing, user can override
-  hrClassName = '', 
-}: SectionHeaderProps) {
+  as = "h2",
+  h2ClassName = '',
+  hrClassName = '',
+}: SectionHeaderProps & { as?: "h1" | "h2" | "h3" }) {
+  const Heading = as;
   return (
     <div>
-      <h2 
-        className={`text-md tracking-wider text-center font-extrabold font-raleway ${h2ClassName}`}
-      >
+      <Heading className={`text-md tracking-wider text-center font-extrabold font-raleway ${h2ClassName}`}>
         {title}
-      </h2>
-      <hr 
-        className={`clear-both mx-auto my-5 h-0 w-[90px] border-0 border-t-[3px] border-solid border-[#8b0000] ${hrClassName}`} 
-      />
+      </Heading>
+      <hr className={`clear-both mx-auto my-5 h-0 w-[90px] border-0 border-t-[3px] border-solid border-[#8b0000] ${hrClassName}`} />
     </div>
   );
 }
--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -75,7 +75,7 @@ export default async function Home() {
       <SchoolsInfo />
 
       <section className="flex flex-col justify-center items-center px-4">
-        <SectionHeader title="TENTANG KAMI" />
+        <SectionHeader title="TENTANG KAMI"/>
         <p className="max-w-4xl mt-6 font-merriweather font-[100] leading-loose tracking-wider text-md text-center">
           Sekolah Kemurnian pertama didirikan dengan nama TK Kemurnian, pada
           tanggal 2 Januari 1978 di Jalan Kemurnian V No. 209, Jakarta Barat.
