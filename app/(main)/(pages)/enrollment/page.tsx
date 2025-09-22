1cdf00b0f1cf31099e84e2899107cb785340d959
MystiaFin <cytrsx01@gmail.com>
2025-09-22 15:42:32 +0700

    initiate enrollment page

--- app/(main)/(pages)/enrollment/page.tsx
+++ app/(main)/(pages)/enrollment/page.tsx
@@ -1,5 +1,21 @@
+import { createClient } from "@/utils/supabase/client";
+import Image from "next/image";
+import QuillRenderer from "@/app/(main)/components/QuillRenderer";
+
 export default async function EnrollmentPage() {
-    return(
-        <div>page</div>
-    )
+  const supabase = createClient();
+
+  const { data, error } = await supabase
+    .from("enrollment")
+    .select("*")
+    .single();
+
+  return (
+    <main>
+      <h1>{data.title}</h1>
+      <Image src={data.image_url} alt="image" width={100} height={100} />
+      <QuillRenderer content={data.body} />
+    </main>
+  );
 }
+
