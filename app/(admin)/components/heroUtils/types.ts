d23c9b7db8fac015743f0cc8b777fd2a576b1f43
MystiaFin <cytrsx01@gmail.com>
2025-10-08 12:08:11 +0700

    remove unused files

--- app/(admin)/components/heroUtils/types.ts
+++
@@ -1,6 +0,0 @@
-export interface Hero {
-  id: number
-  image_urls: string
-  order: number
-  header_text: string
-}
--- app/(admin)/utils/drag.ts
+++
@@ -1,7 +0,0 @@
-
-export function arrayMove<T>(arr: T[], from: number, to: number): T[] {
-  const newArr = [...arr]
-  const [moved] = newArr.splice(from, 1)
-  newArr.splice(to, 0, moved)
-  return newArr
-}
