7e3b8a0393ed74373ff46c24475597074385861e
MystiaFin <cytrsx01@gmail.com>
2025-10-09 10:26:51 +0700

    add iframe of the webpage

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -1,8 +1,16 @@
+const WEB_PAGE = process.env.SITE_DOMAIN!
+
 export default function AdminHome() {
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold">Admin Dashboard</h1>
-      <p className="mt-2 text-gray-600">Welcome to the admin area.</p>
+      <p className="mt-3 text-gray-600">Website Preview</p>
+      <iframe
+        src={WEB_PAGE}
+        className="w-full h-[calc(100vh-100px)] border rounded-lg shadow-sm"
+        loading="lazy"
+        allowFullScreen
+      />
     </div>
   );
 }
