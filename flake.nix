6d8b5a56d0be7e074349db0697c8b33bbd979db8
MystiaFin <cytrsx01@gmail.com>
2026-01-06 08:32:33 +0700

    commit

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Node.js + pnpm + vercel dev shell";
+  description = "Kemurnian Website Development Shell";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -15,14 +15,25 @@
           nodejs
           nodePackages.pnpm
           nodePackages.vercel
+          supabase-cli
+          minio-client
+          postgresql
+          prisma-engines
+          nodePackages.prisma
+          openssl
         ];
 
         shellHook = ''
           echo "node  : $(node -v)"
           echo "pnpm  : $(pnpm -v)"
           echo "vercel: $(vercel --version)"
+          
+          # Prisma Setup for NixOS
+          export PRISMA_SCHEMA_ENGINE_BINARY="${pkgs.prisma-engines}/bin/schema-engine"
+          export PRISMA_QUERY_ENGINE_BINARY="${pkgs.prisma-engines}/bin/query-engine"
+          export PRISMA_FMT_BINARY="${pkgs.prisma-engines}/bin/prisma-fmt"
+          export PRISMA_ENGINES_CHECKSUM_IGNORE_MISSING=1
         '';
       };
     };
 }
-
