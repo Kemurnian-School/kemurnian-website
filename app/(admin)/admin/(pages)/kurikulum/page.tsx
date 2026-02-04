17f98398832463799c493f2afe479e16557afaa2
MystiaFin <cytrsx01@gmail.com>
2026-02-04 15:31:46 +0700

    change the kurikulum create route from /new to /create. use reusable component for the new kurikulum action button

--- app/(admin)/admin/(pages)/kurikulum/page.tsx
+++ app/(admin)/admin/(pages)/kurikulum/page.tsx
@@ -1,5 +1,6 @@
 import KurikulumList from "@admin/components/KurikulumList";
 import { kurikulumRepository } from "@repository/kurikulum";
+import ActionButton from "@admin/components/ActionButton";
 
 export default async function AdminKurikulum() {
   const repo = await kurikulumRepository();
@@ -12,25 +13,7 @@ export default async function AdminKurikulum() {
         <h1 className="text-3xl font-bold text-gray-900">
           Kurikulum Management
         </h1>
-        <a
-          href="/admin/kurikulum/new"
-          className="bg-btn-primary hover:bg-red-primary text-white px-4 py-2 rounded-lg flex items-center space-x-2 transition-colors duration-200"
-        >
-          <svg
-            className="w-5 h-5"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-            ></path>
-          </svg>
-          <span>New Curriculum</span>
-        </a>
+        <ActionButton href="/admin/kurikulum/create" label="+ New Kurikulum" />
       </div>
       <KurikulumList initialKurikulums={kurikulums} />
     </div>
