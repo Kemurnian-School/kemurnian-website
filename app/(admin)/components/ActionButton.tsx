01f933bb585118e8d9cba2636666aa553074a22c
MystiaFin <cytrsx01@gmail.com>
2026-02-12 14:12:56 +0700

    improve admin ActionButton styling

--- app/(admin)/components/ActionButton.tsx
+++ app/(admin)/components/ActionButton.tsx
@@ -9,7 +9,7 @@ export default function ActionButton({ href, label }: ActionButtonProps) {
   return (
     <Link
       href={href}
-      className="mb-6 inline-block px-4 py-2 bg-btn-primary text-white rounded-full hover:bg-red-primary transition-colors"
+      className="mb-6 inline-block px-4 py-2 bg-btn-primary text-white rounded-full hover:bg-red-primary active:bg-red-900 transition-colors"
     >
       {label}
     </Link>
