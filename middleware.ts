fb9ac1f49d59ba53b32aeb8900638d0f8b3cd916
MystiaFin <cytrsx01@gmail.com>
2026-03-09 10:48:13 +0700

    change anon key to publishable key

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { createServerClient } from "@supabase/ssr";
 function createSupabaseClient(request: NextRequest, response: NextResponse) {
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
     {
       cookies: {
         getAll: () => request.cookies.getAll(),
