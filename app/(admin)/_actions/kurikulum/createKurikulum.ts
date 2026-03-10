bd73359a51d1eeda360472c57fc9675f4a3842be
MystiaFin <cytrsx01@gmail.com>
2026-03-10 10:07:07 +0700

    redirect user to /admin/kurikulum after created kurikulum successfully

--- app/(admin)/_actions/kurikulum/createKurikulum.ts
+++ app/(admin)/_actions/kurikulum/createKurikulum.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 import { kurikulumRepository } from "@repository/kurikulum";
 
 export async function uploadKurikulum(formData: FormData) {
@@ -22,7 +23,10 @@ export async function uploadKurikulum(formData: FormData) {
     });
 
     revalidatePath("/");
-    return { success: true };
+    redirect(
+      "/admin/kurikulum?success=" +
+        encodeURIComponent("Kurikulum created successfully"),
+    );
   } catch (error) {
     console.error("Failed to upload kurikulum:", error);
     throw error;
